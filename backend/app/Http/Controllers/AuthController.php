5642ccc5d50f59651077ba9a2472b89646ec05c1
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-02-05 09:58:49 +0900

    Base status 200 helper function & creation AuthController

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponses;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+    public function login()
+    {
+        return $this->ok('Hello, Login!');
+    }
+}
