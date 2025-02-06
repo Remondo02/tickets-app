d6a1005d0097d15d0971e6161bcdac18add1dc17
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-02-06 14:01:47 +0900

    Authentication using tokens

--- backend/routes/api_v1.php
+++ backend/routes/api_v1.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::apiResource('tickets', TicketController::class)->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
