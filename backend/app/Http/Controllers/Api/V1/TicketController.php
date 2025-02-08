05ce57a67c41a7bcbd426a7db626bfa9eeb4ad3f
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-02-08 11:41:42 +0900

    Deleting resources with delete requests

--- backend/app/Http/Controllers/Api/V1/TicketController.php
+++ backend/app/Http/Controllers/Api/V1/TicketController.php
@@ -47,12 +47,18 @@ class TicketController extends ApiController
     /**
      * Display the specified resource.
      */
-    public function show(Ticket $ticket)
+    public function show($ticket_id)
     {
-        if ($this->include('author')) {
-            return new TicketResource($ticket->load('user'));
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            if ($this->include('author')) {
+                return new TicketResource($ticket->load('user'));
+            }
+            return new TicketResource($ticket);
+        } catch (ModelNotFoundException) {
+            return $this->error('Ticket cannot be found', 404);
         }
-        return new TicketResource($ticket);
     }
 
     /**
@@ -66,8 +72,15 @@ class TicketController extends ApiController
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Ticket $ticket)
+    public function destroy($ticket_id)
     {
-        //
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+            $ticket->delete();
+
+            return $this->ok('Ticket successfully deleted');
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found.', 404);
+        }
     }
 }
