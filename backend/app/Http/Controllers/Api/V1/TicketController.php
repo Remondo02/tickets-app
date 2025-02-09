6ad5a5b93a03f0cf02a8afd1498a51d5816c4d1c
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-02-09 10:22:47 +0900

    Replacing resources with put requests

--- backend/app/Http/Controllers/Api/V1/TicketController.php
+++ backend/app/Http/Controllers/Api/V1/TicketController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Resources\V1\TicketResource;
@@ -64,9 +65,30 @@ class TicketController extends ApiController
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTicketRequest $request, Ticket $ticket)
+    public function update(UpdateTicketRequest $request, $ticket_id)
     {
-        //
+        // PATCH
+    }
+
+    public function replace(ReplaceTicketRequest $request, $ticket_id)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            $model = [
+                'title'       => $request->input('data.attributes.title'),
+                'description' => $request->input('data.attributes.description'),
+                'status'      => $request->input('data.attributes.status'),
+                'user_id'     => $request->input('data.relationships.author.data.id'),
+            ];
+
+            $ticket->update($model);
+
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found.', 404);
+        }
+
+        return new TicketResource($ticket);
     }
 
     /**
