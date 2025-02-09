0bb6dc3e81c4af1fc2cf3564a9c301686c833bf4
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2025-02-09 15:21:07 +0900

    Updating resources with patch requests

--- backend/app/Http/Controllers/Api/V1/TicketController.php
+++ backend/app/Http/Controllers/Api/V1/TicketController.php
@@ -34,14 +34,7 @@ class TicketController extends ApiController
             ]);
         }
 
-        $model = [
-            'title'       => $request->input('data.attributes.title'),
-            'description' => $request->input('data.attributes.description'),
-            'status'      => $request->input('data.attributes.status'),
-            'user_id'     => $request->input('data.relationships.author.data.id'),
-        ];
-
-        return new TicketResource(Ticket::create($model));
+        return new TicketResource($request->mappedAttributes());
 
     }
 
@@ -67,7 +60,16 @@ class TicketController extends ApiController
      */
     public function update(UpdateTicketRequest $request, $ticket_id)
     {
-        // PATCH
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            $ticket->update($request->mappedAttributes());
+
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found.', 404);
+        }
+
+        return new TicketResource($ticket);
     }
 
     public function replace(ReplaceTicketRequest $request, $ticket_id)
@@ -75,14 +77,7 @@ class TicketController extends ApiController
         try {
             $ticket = Ticket::findOrFail($ticket_id);
 
-            $model = [
-                'title'       => $request->input('data.attributes.title'),
-                'description' => $request->input('data.attributes.description'),
-                'status'      => $request->input('data.attributes.status'),
-                'user_id'     => $request->input('data.relationships.author.data.id'),
-            ];
-
-            $ticket->update($model);
+            $ticket->update($request->mappedAttributes());
 
         } catch (ModelNotFoundException $exception) {
             return $this->error('Ticket cannot be found.', 404);
